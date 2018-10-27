8d3fc22695dd548cc9418dfaec7f8d1a64d85eef
Sun <22406854+SunTheCourier@users.noreply.github.com>
2018-10-26 23:39:49 -0700

    Upload files

--- Program.cs
+++ Program.cs
@@ -1,15 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using text_msbt;
 
-namespace MSBT_Commit
+namespace MSBT_cmd
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string file = args[0];
+            string entryname = args[1];
+            string label = args[2];
+
+            if (args.Length != 3)
+            {
+                Console.WriteLine("MSBT_cmd.exe {file path} {entry name} {label text}");
+                return;
+            }
+
+            MsbtAdapter CMD = new MsbtAdapter();
+            CMD.Load(file, true);
+
+            bool found = false;
+            foreach (MsbtEntry entry in CMD.Entries)
+            {
+                if (entry.Name == entryname)
+                {
+                    entry.EditedText = label;
+                    found = true;
+                    break;
+                }
+            }
+            CMD.Save();
+            if (found == false) Console.WriteLine("Entry name not found.");
+            Console.WriteLine("Done!");
         }
     }
 }
